9de9d4c4541f619931f9dd920e4e41c0a8807f8d
Praise Idowu <ifeoluwapraise02@gmail.com>
2024-01-10 08:45:59 +0100

    Implemented the functionality

--- listavg.py
+++ listavg.py
@@ -1,9 +1,11 @@
 class ListAverage:
     def __init__(self, lst):
         self.lst = lst.copy()
+        self.total_sum = sum(lst)
 
     def add(self, num):
         self.lst.append(num)
+        self.total_sum += num
 
     def compute_avg(self):
         total = 0
@@ -12,5 +14,12 @@ class ListAverage:
         return total / len(self.lst)
 
     def compute_avg_faster(self):
-        # implement this method
-        pass
+        return self.total_sum / len(self.lst)
+    
+average = ListAverage([1, 3, 5, 7, 9, 11, 13])
+print(average.compute_avg_faster())
+print(average.add(4))
+print(average.add(5))
+print(average)
+print(type(average))
+
